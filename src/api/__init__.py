fdb5382c44963d90c0946ce8217335d716981590
Antonio Fernando <antoniofernandodj@outlook.com>
2025-03-23 03:57:36 -0300

    Add error handler registration to API setup

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 
+
 def create_app():
 
     app = FastAPI()
 
     from src.api import routers
+    from src.api.utils import register_exceptions
 
     app.include_router(routers.event_router)
     app.include_router(routers.calendar_router)
     app.include_router(routers.sharing_router)
     app.include_router(routers.user_router)
     app.include_router(routers.auth_router)
+
+    register_exceptions(app)
 
     return app
