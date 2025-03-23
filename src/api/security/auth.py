7e16399935a9cd3b217977e0234096b4de2343f3
Antonio Fernando <antoniofernandodj@outlook.com>
2025-03-23 03:58:27 -0300

    Refactor ApiAuthService to use class methods

--- src/api/security/auth.py
+++ src/api/security/auth.py
@@ -16,17 +16,19 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class ApiAuthService:
-    def __init__(self):
-        self.auth_service = AuthService()
 
-    def autenticar_usuario(self, usuario: User, senha: str) -> bool:
-        return self.auth_service.autenticar_usuario(usuario, senha)
+    @classmethod
+    def autenticar_usuario(cls, usuario: User, senha: str) -> bool:
+        auth_service = AuthService()
+        return auth_service.autenticar_usuario(usuario, senha)
 
-    def criar_token_acesso(self, user_id: str):
+    @classmethod
+    def criar_token_acesso(cls, user_id: str):
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         return criar_token_acesso({"sub": user_id}, expires_delta=access_token_expires)
 
-    async def obter_usuario_pelo_token(self, token: str):
+    @classmethod
+    async def obter_usuario_pelo_token(cls, token: str):
         try:
             user_id = verificar_token_acesso(token)
         except JWTError:
