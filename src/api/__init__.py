4867c32ad9197258c32238898dc7d63508c3b553
Antonio Fernando <antoniofernandodj@outlook.com>
2025-03-30 05:12:49 -0300

    Add database schema creation during FastAPI startup

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,10 +1,21 @@
+
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.database.entities import metadata
+from src.database.entities import sync_engine
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    metadata.create_all(bind=sync_engine)
+    yield
 
 
 def create_app():
 
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     from src.api import routers
     from src.api.utils import register_exceptions
@@ -24,5 +35,7 @@ def create_app():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    print(app.state)
 
     return app
